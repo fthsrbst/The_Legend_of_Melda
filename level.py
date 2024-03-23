8657d26aced2f677ddd99267baed50389160a20f
FATIH\FATIH <fatihxserbest@gmail.com>
2024-03-23 21:30:09 +0300

    map updated

--- level.py
+++ level.py
@@ -14,14 +14,14 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-                if col == 'p':
-                    self.player=Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+       # for row_index, row in enumerate(WORLD_MAP):
+       #     for col_index, col in enumerate(row):
+       #         x = col_index * TILESIZE
+       #         y = row_index * TILESIZE
+       #         if col == 'x':
+       #            Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+       #         if col == 'p':
+                    self.player=Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
 
 
     def run(self):
@@ -37,12 +37,19 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        #zemin yaratılıyor
+        self.floor_surf = pygame.image.load('graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft=(0,0))
     def custom_draw(self,player):
 
         #getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y =player.rect.centery - self.half_height
-#for sprite in self.sprites():
+
+        #ZEMİN ÇİZİLİYOR
+        floor_offset_pos =self.floor_rect.topleft- self.offset
+        self.display_surface.blit(self.floor_surf ,floor_offset_pos)
+        #for sprite in self.sprites():
         for sprite in sorted(self.sprites(),key=lambda sprite:sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image,offset_pos)

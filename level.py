c85e58e84045d1fe77a567e60835d85686f2e89d
FATIH\FATIH <fatihxserbest@gmail.com>
2024-03-24 03:38:05 +0300

    map updated

--- level.py
+++ level.py
@@ -16,15 +16,28 @@ class Level:
 
     def create_map(self):
         layouts={
-            'boundary': import_csv_layout('map/map_FloorBlocks.csv')
+            'boundary': import_csv_layout('map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('map/map_Grass.csv'),
+            'object': import_csv_layout('map/map_Objects.csv')
         }
+        graphics = {
+            'grass' : import_folder('/graphics/Grass')
+        }
+
         for style, layout in layouts.items():
-            for row_index, row in enumerate(WORLD_MAP):
+            for row_index, row in enumerate(layout):
                 for col_index, col in enumerate(row):
-                    x = col_index * TILESIZE
-                    y = row_index * TILESIZE
-                    if style == 'boundary':
-                        Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'invisible')
+                    if col != '-1':
+                       x = col_index * TILESIZE
+                       y = row_index * TILESIZE
+                       if style == 'boundary':
+                        Tile((x, y), [self.obstacle_sprites], 'invisible')
+                       if style == 'grass':
+
+                           pass
+                       if style == 'object':
+
+                           pass
 
             self.player = Player((2000, 1430), [self.visible_sprites], self.obstacle_sprites)
 
